59420e35536e75c9c9c268865694ce9434bb864e
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-12-02 10:15:54 -0500

    Commit 4

---
+++ vite-project/js/main.js
@@ -0,0 +1,6 @@
+import "../styles/style.css";
+import javascriptLogo from "../javascript.svg";
+import { setupCounter } from "../counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <h1>Hello</h1>`;
