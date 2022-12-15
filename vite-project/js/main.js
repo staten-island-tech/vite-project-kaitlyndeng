e1a6a2aadb773c667d5069718717db55e7787201
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-12-15 10:15:53 -0500

    Commit ??

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,25 @@
 import "../styles/style.css";
 import { candy } from "./candy";
-document.querySelector("box").innerHTML = `
-  <h2>Yum</h2>`;
-console.log(candy);
+
+const DOMSelectors = {
+  option: document.querySelector(".option"),
+  name: document.querySelector(".name"),
+  url: document.querySelector(".image"),
+  desc: document.querySelector(".desc"),
+  price: document.querySelector(".price"),
+};
+
+
+function insert () {
+  DOMSelectors.option.insertAdjacentHTML {
+    "beforeend",
+    `<div class="option">
+    <h3 class="name">${name}</h3>
+    <img class="image" src="${url}">
+    <h4 class="desc">${desc}</h4>
+    <h5 class="price">${price}</h5>
+    <h5>In Stock</h5>
+  </div> `
+  }
+  
+}
