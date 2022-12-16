42490da0fc29f4610054b93922184a80fe33f041
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-12-16 09:33:22 -0500

    commit

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -9,17 +9,16 @@ const DOMSelectors = {
   price: document.querySelector(".price"),
 };
 
-
-function insert () {
-  DOMSelectors.option.insertAdjacentHTML {
+function insert() {
+  DOMSelectors.option.insertAdjacentHTML;
+  {
     "beforeend",
-    `<div class="option">
+      `<div class="option">
     <h3 class="name">${name}</h3>
     <img class="image" src="${url}">
     <h4 class="desc">${desc}</h4>
     <h5 class="price">${price}</h5>
     <h5>In Stock</h5>
-  </div> `
+  </div> `;
   }
-  
 }
