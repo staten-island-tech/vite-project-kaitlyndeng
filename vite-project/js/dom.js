c01edc2964f6194ed3170a9a1189056e0b55be7c
kaitlyndeng <kaitlyndeng20@gmail.com>
2022-12-19 10:16:39 -0500

    Commit 13

---
+++ vite-project/js/dom.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+  option: document.querySelector(".option"),
+  name: document.querySelector(".name"),
+  url: document.querySelector(".image"),
+  desc: document.querySelector(".desc"),
+  price: document.querySelector(".price"),
+};
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,23 +1,24 @@
 import "../styles/style.css";
 import { candy } from "./candy";
 
-const DOMSelectors = {
-  option: document.querySelector(".option"),
-  name: document.querySelector(".name"),
-  url: document.querySelector(".image"),
-  desc: document.querySelector(".desc"),
-  price: document.querySelector(".price"),
-};
-
 candy.forEach((card) =>
   DOMSelectors.option.insertAdjacentHTML(
     "beforeend",
     `<div class="option">
-    <h3 class="name">${name}</h3>
-    <img class="image" src="${url}">
-    <h4 class="desc">${desc}</h4>
-    <h5 class="price">${price}</h5>
+    <h3 class="name">${card.name}</h3>
+    <img class="image" src="${card.url}">
+    <h4 class="desc">${card.desc}</h4>
+    <h5 class="price">${card.price}</h5>
     <h5>In Stock</h5>
   </div> `
   )
 );
+let outOfStock = 0;
+
+function filterByID(candy) {
+  if (Number.isFinite(candy.id) && candy.id !== 0) {
+    return true;
+  }
+  outOfStock++;
+  return false;
+}
